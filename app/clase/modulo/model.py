ac0cacbefc710d8ff8c866661bd21fdf86e318a0
Franmartin09 <franmartinaguilar@gmail.com>
2024-06-26 17:16:41 +0200

    requirements & clase inside app dir

---
+++ app/clase/modulo/model.py
@@ -0,0 +1,51 @@
+import  os
+os.environ['TF_ENABLE_ONEDNN_OPTS'] = '0'
+import numpy as np
+import pandas as pd
+from tensorflow.keras.models import load_model
+import pickle 
+
+class Model:
+    def __init__(self):
+        self.model = load_model("app/clase/modulo/rnn_final_final.h5")
+        pkl_file = open('app/clase/modulo/label_encoders.pkl', 'rb')
+        self.encoders = pickle.load(pkl_file)
+        pkl_file.close()
+        self.keys_order = [
+        "src_ip", "src_port", "dst_ip", "dst_port", "proto", "service", "duration",
+        "src_bytes", "dst_bytes", "conn_state", "missed_bytes", "src_pkts",
+        "src_ip_bytes", "dst_pkts", "dst_ip_bytes", "dns_query", "dns_qclass",
+        "dns_qtype", "dns_rcode", "dns_AA", "dns_RD", "dns_RA", "dns_rejected",
+        "ssl_version", "ssl_cipher", "ssl_resumed", "ssl_established", "ssl_subject",
+        "ssl_issuer", "http_trans_depth", "http_method", "http_uri", "http_version",
+        "http_request_body_len", "http_response_body_len", "http_status_code",
+        "http_user_agent", "http_orig_mime_types", "http_resp_mime_types", "http_referrer", "weird_name",
+        "weird_addl", "weird_notice"
+        ]
+        print("ModelClass inicialized!")
+    
+    
+    def predict(self, data):
+        return self.model.predict(data)
+    
+    def preproces(self, data):
+        data_values=list(data.values())
+        data_values = {key: [data_values[idx]] for idx, key in enumerate(self.keys_order)}
+        data_df = pd.DataFrame(data_values)
+
+        for column in data_df.columns:
+            # print(column)
+            if column in self.encoders:
+                # print(column)
+                encoder = self.encoders[column]
+                new_values = data_df[column].apply(lambda x: x not in encoder.classes_)
+                if new_values.any():
+                    # Añadir nuevas clases al encoder
+                    new_classes = list(set(data_df[column][new_values]))
+                    updated_classes = list(encoder.classes_) + new_classes
+                    encoder.classes_ = np.array(updated_classes)
+                
+                data_df[column] = encoder.transform(data_df[column])
+
+        return (np.array(data_df))
+
--- app/main.py
+++ app/main.py
@@ -1,24 +1,22 @@
+#library imports
 import customtkinter as ctk
 import os, sys
 sys.path.append(os.path.dirname(os.path.dirname(os.path.realpath(__file__))))
-from clase.visual.nids_ai_visual import NIDS_AI_Visual
-from clase.read.readlog import ReadLog
-from clase.modulo.model import Model
-
-def install_requirements():
-    #comadnos para requirements
-    print("·")
+from app.clase.visual.nids_ai_visual import NIDS_AI_Visual
+from app.clase.read.readlog import ReadLog
+from app.clase.modulo.model import Model
 
 def main():
-    #VISUAL
     root = ctk.CTk()
+    #Model Class
     model = Model()
+    #Visual Class
     app = NIDS_AI_Visual(root)
+    #ReadLog Class
     read = ReadLog()
     
     read.read_log(app, model)
     root.mainloop()
 
 if __name__ == "__main__":
-    # install_requirements()
     main()
